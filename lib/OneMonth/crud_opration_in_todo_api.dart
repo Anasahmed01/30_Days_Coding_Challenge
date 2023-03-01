aa53cbed89ae19bac3701de5b630a15fef7fa466
anasahmed01 <zainkhilji2323@gmail.com>
2023-03-02 00:31:54 +0500

    add_widgets

--- lib/OneMonth/crud_opration_in_todo_api.dart
+++ lib/OneMonth/crud_opration_in_todo_api.dart
@@ -535,3 +535,41 @@
 // .
 // .
 // .
+// >>>>>>>>>>>>>>>>>>>>>>>>>>WIDGETS<<<<<<<<<<<<<<<<<<<<<<
+// .
+// .
+// .
+// .
+// .
+// .
+// textfield({required hintText, required controllar}) {
+//   return TextField(
+//     decoration: InputDecoration(
+//         hintText: hintText,
+//         hintStyle: const TextStyle(
+//           color: Color.fromARGB(255, 0, 0, 0),
+//         ),
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(5.0),
+//         ),
+//         fillColor: Colors.transparent,
+//         filled: true),
+//     style: const TextStyle(
+//       color: Color.fromARGB(255, 0, 0, 0),
+//     ),
+//     controller: controllar,
+//   );
+// }
+// .
+// .
+// .
+// .
+// .
+// .
+// >>>>>>>>>>>>>>>>>>>>>>>>>>WIDGETS END<<<<<<<<<<<<<<<<<<<<<<
+// .
+// .
+// .
+// .
+// .
+// .
