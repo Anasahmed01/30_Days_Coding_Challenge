59af9ee2e7401ea9a19b8306714f75ce5dc3f46e
anasahmed01 <zainkhilji2323@gmail.com>
2023-03-03 03:08:51 +0500

    Complet_Dot_indicator

--- lib/OneMonth/dot_indicator.dart
+++ lib/OneMonth/dot_indicator.dart
@@ -176,3 +176,44 @@
 // .
 // .
 // >>>>>>>>>>>>>>>>>>>>>>>>>>third_VIEW END<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// >>>>>>>>>>>>>>>>>>>>>>>>>>FOURTH_VIEW START<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// .
+// .
+// .
+// .
+// .
+// .
+// class FourthView extends StatelessWidget {
+//   const FourthView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.transparent,
+//       body: Padding(
+//         padding: const EdgeInsets.all(40.0),
+//         child: ClipRRect(
+//           borderRadius: BorderRadius.circular(10),
+//           child: Container(
+//             color: Colors.deepPurple[400],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+// .
+// .
+// .
+// .
+// .
+// .
+// .
+// >>>>>>>>>>>>>>>>>>>>>>>>>>FOURTH_VIEW END<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
