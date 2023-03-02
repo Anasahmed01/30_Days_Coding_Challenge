fe05b58cf5e700e4e2ed43138ad7c0497314b966
anasahmed01 <zainkhilji2323@gmail.com>
2023-03-03 01:22:25 +0500

    add_dot_Indicator

---
+++ lib/OneMonth/dot_indicator.dart
@@ -0,0 +1,61 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>DOT INDECATOR START<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// .
+// .
+// .
+// .
+// .
+// .
+// import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+// class DotIndicatorView extends StatefulWidget {
+//   const DotIndicatorView({super.key});
+
+//   @override
+//   State<DotIndicatorView> createState() => _DotIndicatorViewState();
+// }
+
+// class _DotIndicatorViewState extends State<DotIndicatorView> {
+//   final controller = PageController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.deepPurple[200],
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//         children: [
+//           SizedBox(
+//             height: 500,
+//             child: PageView(
+//               controller: controller,
+//               children: const [
+//                 FirstView(),
+//                 SecondView(),
+//                 ThirdView(),
+//                 FourthView(),
+//               ],
+//             ),
+//           ),
+//           SmoothPageIndicator(
+//             controller: controller,
+//             count: 4,
+//             effect: const JumpingDotEffect(
+//                 dotColor: Colors.black,
+//                 activeDotColor: Colors.deepPurple,
+//                 dotWidth: 30,
+//                 dotHeight: 30,
+//                 spacing: 16,
+//                 verticalOffset: 30),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+// .
+// .
+// .
+// .
+// .
+// .
+// >>>>>>>>>>>>>>>>>>>>>>>>>>DOT INDECATOR START<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
